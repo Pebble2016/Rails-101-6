a48f957717be97ab4fcb09404765169dfaa510a5
ypj <ypj2005@126.com>
2017-09-05 00:54:44 +0800

    添加分页功能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy]
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
 
   def new
     @group = Group.find(params[:group_id])
@@ -20,10 +25,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    # @group = Group.find(params[:group_id]) 这是不需要的
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to account_posts_path, notice: "Update success!"
+  end
+
+  def destroy
+    # @group = Group.find(params[:group_id]) 这个也不需要
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted!"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
